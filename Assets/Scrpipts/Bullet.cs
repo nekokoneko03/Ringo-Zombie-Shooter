321cf5fc97651f9019a499b0a6178c5b3364fd31
okaerinasai39 <ryousuke1039@gmail.com>
2022-10-19 23:33:43 +0900

    Update Bullet.cs

--- Assets/Scrpipts/Bullet.cs
+++ Assets/Scrpipts/Bullet.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private AudioClip shotSound;
+
+    public AudioClip ShotSound { get => shotSound; } 
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
